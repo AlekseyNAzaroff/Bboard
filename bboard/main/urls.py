dc67f5a61cc2d4f98c14a0889adac0808dfc7be9
Alexey Nazarov <mralexman@yandex.ru>
2023-08-23 12:00:57 +0300

    added login

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView
-
+from .views import index, other_page, BBLoginView, BBLogoutView, profile, \
+    ChangeUserInfoView, BBPasswordChangeView
 
 app_name = 'main'
 urlpatterns = [
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/password/change/', BBPasswordChangeView.as_view(),
+         name='password_change'),
+    path('accounts/profile/change/', ChangeUserInfoView.as_view(),
+         name='profile_change'),
+    path('accounts/profile/', profile, name='profile'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index'),
